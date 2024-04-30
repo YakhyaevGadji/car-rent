f8c07019aeb5e0bcc403b14e708d02b8e856f3f6
YakhyaevGadji <gadji180105@gmail.com>
2024-05-01 01:23:41 +0300

    add: section recommendation in page home

--- src/entities/carblock/ui/CarBlock.tsx
+++ src/entities/carblock/ui/CarBlock.tsx
@@ -5,7 +5,9 @@ const CarBlock: React.FC = () => {
     return (
         <li className="car">
             <img className="car__img" src="https://s3-eu-west-1.amazonaws.com/localrent.images/cars/image_titles/000/041/378/original/MG-ZS-2020-red_(1).jpg?1680090691" alt="" />
-            <h3 className="car__title">KIA k5</h3>
+            <p className="car__title">KIA k5</p>
+            <p className="car__details">Автомат, 1.5л</p>
+            <p className="car__price">25$</p>
         </li>
     );
 }
--- src/widgets/sliderCar/ui/SliderCar.tsx
+++ src/widgets/sliderCar/ui/SliderCar.tsx
@@ -1,11 +1,21 @@
+import React from "react";
 import { CarBlock } from "../../../entities/index";
+import "./sliderCar.scss";
 
-const SliderCar = () => {
+const SliderCar: React.FC = () => {
     return (
-        <ul className="slider-car">
-            <CarBlock/>
-        </ul>
+        <section className="slider-car">
+            <div className="container">
+                <h2 className="slider-car__title">Автомат</h2>
+                <ul className="slider-car__list">
+                    <CarBlock />
+                    <CarBlock />
+                    <CarBlock />
+                    <CarBlock />
+                </ul>
+            </div>
+        </section>
     );
 }
- 
+
 export default SliderCar;
