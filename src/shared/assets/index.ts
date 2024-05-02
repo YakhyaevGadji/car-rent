b6690ebbba088d753e9ae30ccb92c28d24c7ab9b
YakhyaevGadji <gadji180105@gmail.com>
2024-05-02 13:16:05 +0300

    change: section advantage

--- src/shared/assets/index.ts
+++ src/shared/assets/index.ts
@@ -1,5 +1,10 @@
 import depoIcon from "./icons/depist-icon.png";
+import agreementIcon from "./icons/agreement-icon.png";
+import likeIcon from "./icons/like-icon.png";
 
 export const icons = {
-    depoIcon
-};
+    depoIcon,
+    agreementIcon,
+    likeIcon
+}
+
--- src/widgets/advantage/ui/Advantage.tsx
+++ src/widgets/advantage/ui/Advantage.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { icons } from "../../../shared/assets";
 import "./advantage.scss";
 
 const Advantage: React.FC = () => {
@@ -8,7 +9,19 @@ const Advantage: React.FC = () => {
                 <h2 className="advantage__title">Преимущество</h2>
                 <ul className="advantage__list">
                     <li className="advantage__item">
-                        <img src="" alt="" />
+                        <img className="advantage__img" src={icons.agreementIcon} alt="" />
+                        <h3 className="advantage__subtitle">Без мелкого шрифта</h3>
+                        <p className="advantage__paragraph">Все условия и все цены мы держим на виду. Никаких скрытых платежей, сборов, налогов. Вы сразу видите конечную цену, которая не изменится, пока вы оформляете заказ.</p>
+                    </li>
+                    <li className="advantage__item">
+                        <img className="advantage__img" src={icons.depoIcon} alt="" />
+                        <h3 className="advantage__subtitle">Низкие депозиты</h3>
+                        <p className="advantage__paragraph">В международных прокатах депозиты достигают нескольких тысяч евро, которые блокируются на карте клиента. У нас депозиты, в среднем, 300 евро, а есть машины и без депозита.</p>
+                    </li>
+                    <li className="advantage__item">
+                        <img className="advantage__img" src={icons.likeIcon} alt="" />
+                        <h3 className="advantage__subtitle">Честная оценка повреждений</h3>
+                        <p className="advantage__paragraph">Мы оцениваем повреждения в присутствии клиента. Наши цены в 2-3 раза ниже, чем у международных прокатов. И мы никогда не списываем деньги с карты клиента после сдачи автомобиля.</p>
                     </li>
                 </ul>
             </div>
