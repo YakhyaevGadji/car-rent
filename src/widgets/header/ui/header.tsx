c6a9c443765e3ea45b473bb15d5628e6e215a476
YakhyaevGadji <gadji180105@gmail.com>
2024-08-21 14:39:07 +0300

    add: mobile adoptive

--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -9,29 +9,47 @@ export const Header: React.FC = () => {
     const [ toggleBurger, setToggleBurger] = React.useState(false);
     const sessionName = sessionStorage.getItem('name');
 
+    //Удалет и добавляет в body класс при клике на бургер
+    const handleToggle = () => {
+        setToggleBurger(!toggleBurger);
+
+        let bodyClassName = document.body;
+
+        if(bodyClassName.className !== 'lock') {
+            bodyClassName.className = 'lock';
+        }else {
+            bodyClassName.classList.remove('lock');
+        }
+    }
+
     return (
-        <header className="header">
-            <div className="container inner">
-                <NavLink className="header__logo" to="/">RI</NavLink>
-                <nav className="header__nav">
-                    <ul className="header__list">
-                        <li className="header__list-item">
-                            <NavLink className="header__list-link" to="/">Домой</NavLink>
-                        </li>
-                        <li className="header__list-item">
-                            <NavLink className="header__list-link" to="/Cars">Машины</NavLink>
-                        </li>
-                        <li className="header__list-item">
-                            <NavLink className="header__list-link" to="/About">О нас</NavLink>
-                        </li>
-                    </ul>
-                </nav>
-                <button onClick={() => setToggleBurger(!toggleBurger)} className={`header__burger ${toggleBurger && `header__burger--active`}`}>
-                    <span></span>
-                </button>
-                {sessionName || isLogged ? <ProfileHeader sessionName={sessionName} user={user}/> : <NavLink className="header__sign" to="/login">Sign / In</NavLink>}
-            </div>
-        </header>
+        <>
+            <header className="header">
+                <div className="container inner">
+                    <NavLink className="header__logo" to="/">RI</NavLink>
+                    <nav className={`header__nav ${toggleBurger ? `header__nav_active` : ''}`}>
+                        <ul className="header__list">
+                            <li className="header__list-item">
+                                <NavLink onClick={handleToggle} className="header__list-link" to="/">Домой</NavLink>
+                            </li>
+                            <li className="header__list-item">
+                                <NavLink onClick={handleToggle} className="header__list-link" to="/Cars">Машины</NavLink>
+                            </li>
+                            <li className="header__list-item">
+                                <NavLink onClick={handleToggle} className="header__list-link" to="/About">О нас</NavLink>
+                            </li>
+                        </ul>
+                    </nav>
+
+                    <button onClick={handleToggle} className={`header__burger ${toggleBurger ? `header__burger--active` : ''}`}>
+                        <span></span>
+                    </button>
+                    {sessionName || isLogged ? <ProfileHeader sessionName={sessionName} user={user}/> :
+                        <NavLink className="header__sign" to="/login">Sign / In</NavLink>}
+                </div>
+            </header>
+            <div onClick={() => handleToggle()} className={`header__back ${toggleBurger ? `header__back_active` : ''}`}></div>
+        </>
     );
 }
 
